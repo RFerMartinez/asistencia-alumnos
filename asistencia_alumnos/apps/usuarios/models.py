f6a8d82a14bcc6765e6230f2fa91d1431f2e6a56
Fer Martínez <ricardofernandomartinez0@gmail.com>
2024-05-21 00:37:26 -0300

    se agrego las app de 'asistencia' y  la app de 'clases'

--- asistencia_alumnos/apps/usuarios/models.py
+++ asistencia_alumnos/apps/usuarios/models.py
@@ -4,6 +4,8 @@ from django.db import models
 
 class Usuario(AbstractUser):
     biografia = models.CharField(max_length=255, null=True, blank=True)
+    # Lo anterior se tiene que actualizar en una tabla 'Usuario' ya existente
+    # para esto, se crea un script en python con el comando 'makemigrations'. Esto luego se convierte en un sql
 
     def __str__(self):
         return f"{self.first_name}, {self.last_name}, ({self.username})"
--- asistencia_alumnos/apps/usuarios/urls.py
+++ asistencia_alumnos/apps/usuarios/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 app_name = "usuarios"
+'''
+Lo anterior se hace ya que puede haber varias urls que se dirigan a 'listar-todos', puede ser 'listar-todos' de
+materias, de alumnos, etc. Para ello se le da un nombre un nombre 'app_name= "usuarios"'
+'''
 
 urlpatterns = [
     path('listar-todos/', views.listar_usuarios, name='listar_todos'),
