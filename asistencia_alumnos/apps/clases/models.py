cb33ce8e9ba2dd3276a58131b2fc76d21d1fa79f
Fer Martínez <ricardofernandomartinez0@gmail.com>
2024-05-22 01:37:30 -0300

    Se creo la tabla de 'clase' de la app clases

--- asistencia_alumnos/apps/clases/models.py
+++ asistencia_alumnos/apps/clases/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Clase(models.Model):
+    fecha = models.DateField()
+
+
+'''
+Lo siguiente es para darle un nombre a la tabla, en vez del nombre que crea Django por defecto
+
+Djando crea una tabla de migraciones, para verificar las migraciones que se hicieron
+
+class NuevaTabla(models.Model):
+    pass
+
+    class Meta:
+        db_table = 'nueba_tabla'
+
+'''
