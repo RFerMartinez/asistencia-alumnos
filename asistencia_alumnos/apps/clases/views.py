3f6e33c56ede6eef13400626ce6ae874c832a7dc
Fer Martínez <ricardofernandomartinez0@gmail.com>
2024-05-25 15:48:45 -0300

    Listado de clases

--- asistencia_alumnos/apps/clases/views.py
+++ asistencia_alumnos/apps/clases/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from apps.usuarios.models import Usuario
+from .models import Clase
 
 # Create your views here.
 def listar_clases(request):
     
     ctx = {
+        'clases': Clase.objects.all().order_by("fecha")
     }
     return render(request=request, template_name='clases/lista.html', context=ctx)
